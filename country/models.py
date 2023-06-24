d45e89b33054a161f7f248d0c7b4883e1fc19717
Amr Salah <amr.abdoullah@gmail.com>
2023-06-24 22:00:28 +0300

    Country is created

--- country/models.py
+++ country/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
--- country/views.py
+++ country/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Country
 # Create your views here.
+
+def country_list(request):
+    countries = Country.objects.all()
+    return render(request, 'country_list.html', {'countries': countries})
+
+def country_create(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        code = request.POST['code']
+        country = Country(name=name, code=code)
+        country.save()
+        return redirect('country_list')
+    return render(request, 'country_create.html')
+
+def country_update(request, pk):
+    country = Country.objects.get(pk=pk)
+    if request.method == 'POST':
+        country.name = request.POST['name']
+        country.code = request.POST['code']
+        country.save()
+        return redirect('country_list')
+    return render(request, 'country_update.html', {'country': country})
+
+def country_delete(request, pk):
+    country = Country.objects.get(pk=pk)
+    if request.method == 'POST':
+        country.delete()
+        return redirect('country_list')
+    return render(request, 'country_delete.html', {'country': country})
