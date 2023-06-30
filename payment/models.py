f34190964c6782be9d607ce1bce4bfdbfdee8e68
MarwaAbdelAal <marwaabdelaal112@gmail.com>
2023-06-30 19:33:38 +0300

    updated payment model with optional foreign keys

--- payment/models.py
+++ payment/models.py
@@ -1,13 +1,40 @@
 from django.db import models
-from user.models import User
+from flight.models import Flight_Reservation
+from hotelReservation.models import StayReservation
+from django.core.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your models here.
+
 class Payment(models.Model):
-    payment_id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    # flightResId = models.ForeignKey('flight.FlightReservation', on_delete=models.CASCADE) # flightResId
-    # staysResId = models.ForeignKey('stays.StaysReservation', on_delete=models.CASCADE) # staysResId
+    payment_id = models.CharField(max_length=255, null=True, blank=True)
+    flightResId = models.ForeignKey(
+        Flight_Reservation, on_delete=models.CASCADE, null=True, blank=True)
+    stayResId = models.ForeignKey(
+        StayReservation, on_delete=models.CASCADE, null=True, blank=True)
     status = models.BooleanField(default=False)
     
+    def get_stay_by_id(self, pk):
+        try:
+            return StayReservation.objects.get(pk=pk)
+        except StayReservation.DoesNotExist:
+            return Response({'error': 'No hotel reservation found'}, status=status.HTTP_404_NOT_FOUND)
+        
+
+    # one of the two foriegn keys should be null and the other should be not null
+    def clean(self):
+        if self.flightResId is None and self.stayResId is None:
+            raise ValidationError(
+                'Both flightResId and stayResId cannot be null')
+        if self.flightResId is not None and self.stayResId is not None:
+            raise ValidationError(
+                'Both flightResId and stayResId cannot be not null')
+
+    # override model's save method to make it always call the clean() method before triggering the Model class save method
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(Payment, self).save(*args, **kwargs)
+
     def __str__(self):
-        return str(self.payment_id)
+        # get the user's email from the flight reservation or the hotel reservation
+        return f'{self.flightResId.user.email if self.flightResId is not None else self.stayResId.user.email} - {self.payment_id}'
