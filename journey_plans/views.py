3220d105c7963fb65fe42ea679435086a6e94339
EngAmrs <116183993+EngAmrs@users.noreply.github.com>
2023-07-04 18:34:46 +0300

    Update views.py

--- journey_plans/views.py
+++ journey_plans/views.py
@@ -22,7 +22,7 @@ def get_trip_plans(request):
         'destination':  last_stay.hotel.city + ',' + last_stay.hotel.countryId.code
     }
     headers = {
-        'X-RapidAPI-Key': '704287766amshc5b664ff1150592p1141b8jsn0d2300f0bb11',
+        'X-RapidAPI-Key': '0690936387msha83dcad864a8b9ep1153b8jsncc728f419c2b',
         'X-RapidAPI-Host': 'ai-trip-planner.p.rapidapi.com'
     }
     
