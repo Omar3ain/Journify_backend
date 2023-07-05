dcebad47325e41947c5fd99e11c7fcfac8a6ed25
EngAmrs <116183993+EngAmrs@users.noreply.github.com>
2023-07-05 08:49:26 +0300

    Update urls.py

--- country/urls.py
+++ country/urls.py
@@ -4,5 +4,5 @@ from .views import CountryAPIView, CountryDetailAPIView, get_geolocation
 urlpatterns = [
     path('', CountryAPIView.as_view(), name='list_country'),
     path('<int:pk>/', CountryDetailAPIView.as_view(), name='detail_country'),
-    # path('geolocation/', get_geolocation, name='get_geolocation'),
+    path('geolocation/', get_geolocation, name='get_geolocation'),
 ]
